151271120f0ef0e91e994b49d64b555a3fa0b6ea
Paracosms <92565929+Paracosms@users.noreply.github.com>
2025-07-19 01:14:16 -0700

    Add volume control slider and integrate with audio
    Introduces a Volume HSlider to the main scene, with a new script (volume.gd) that updates the global volume value. Audio playback functions now use Globals.volume for volume_db, and Globals variables are adjusted for lives and baseYield. This enables user-adjustable sound volume in the game.

--- scripts/main.gd
+++ scripts/main.gd
@@ -133,6 +133,7 @@ func playChipSound():
 	print("chip")
 	var soundPlayer = AudioStreamPlayer.new()
 	soundPlayer.stream = chipAudio.pick_random()
+	soundPlayer.volume_db = Globals.volume
 	add_child(soundPlayer)
 	soundPlayer.play()
 	soundPlayer.finished.connect(soundPlayer.queue_free)
@@ -140,6 +141,7 @@ func playChipSound():
 func playExplosionSound():
 	var soundPlayer = AudioStreamPlayer.new()
 	soundPlayer.stream = explosionAudio.pick_random()
+	soundPlayer.volume_db = Globals.volume
 	add_child(soundPlayer)
 	soundPlayer.play()
 	soundPlayer.finished.connect(soundPlayer.queue_free)
