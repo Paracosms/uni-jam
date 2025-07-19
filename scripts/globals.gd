15035e79c90357b1b307954931056b17f57a4a9b
Nathaniel Greisen <nlgreisen@gmail.com>
2025-07-19 11:38:57 -0700

    Add binoculars camera upgrade and controls
    Introduces a new binoculars camera feature that expands view range based on cursor location. Adds related UI button, global state, and logic for enabling/disabling the upgrade, and integrates the camera scene into the main scene. Also refactors shop state management and adjusts asteroid spawn parameters for improved gameplay balance.

--- scripts/globals.gd
+++ scripts/globals.gd
@@ -2,8 +2,10 @@ extends Node2D
 
 var lives : int = 100
 var starPoints : int = 0
+var shopOpened : bool = false
 var clickDamage : int = 1
 var baseYield : int = 100
 var yieldMultiplier : int = 1
 var critMultiplier : int = 1 # Such that a critMultiplier of n will yield n+1 starpoints
 var critEnabled : bool = false
+var binocularsEnabled : bool = false
